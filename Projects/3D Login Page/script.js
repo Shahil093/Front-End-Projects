5f677f9405d0925701d6d1a75ce9b26be796432f
MD SHAHIL <96812916+Shahil093@users.noreply.github.com>
2023-06-04 10:17:07 +0530

    Update script.js

--- Projects/3D Login Page/script.js
+++ Projects/3D Login Page/script.js
@@ -1,6 +1,42 @@
-const signUpBtn = document.querySelector(".signup-btn");
-const signInBtn = document.querySelector(".signin-btn");
-const formsWrapper = document.querySelector(".forms-wrapper");
+var SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+var recognition = new SpeechRecognition();
+recognition.lang = 'hi-IN';
+
+var Textbox = $("#textarea");
+var instructions = $("#instructions");
+var signUpBtn = document.querySelector(".signup-btn");
+var signInBtn = document.querySelector(".signin-btn");
+var formsWrapper = document.querySelector(".forms-wrapper");
+
+var Content = "";
+
+recognition.continuous = true;
+
+recognition.onresult = function (event) {
+  var current = event.resultIndex;
+  var transcript = event.results[current][0].transcript;
+  Content += transcript;
+  Textbox.val(Content);
+};
+
+$("#start").on("click", function (e) {
+  if ($(this).text() == "Click here to Stop Recording") {
+    $(this).html("Click here to Start Recording");
+    $("#instructions").html("");
+    recognition.stop();
+  } else {
+    $(this).html("Click here to Stop Recording");
+    $("#instructions").html("Try Speaking, Voice Recognition is On, Contents will be displayed below");
+    if (Content.length) {
+      Content += " ";
+    }
+    recognition.start();
+  }
+});
+
+Textbox.on("input", function () {
+  Content = $(this).val();
+});
 
 signUpBtn.addEventListener("click", (e) => {
   e.preventDefault();
